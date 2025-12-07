b055ce16e93f918a0d46382808f994730ca5d4e1
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-12-07 10:04:13 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] num = new int[12];
+
+        System.out.println("Vaya introduciendo números enteros y pulsando INTRO: ");
+
+        for (int i = 0; i < 12; i++) {
+            num[i] = Integer.parseInt(System.console().readLine());
+        }
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+
+        int ultimo = num[num.length - 1];
+
+        for (int i = num.length - 1; i > 0; i--) {
+            num[i] = num[i - 1];
+        }
+
+        num[0] = ultimo;
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
     }
 }
